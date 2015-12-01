5f8b18f9b13e1d0d057c8b1b5761a463bc63f44e
Alexander <alexander.matsaylo@gmail.com>
2015-11-30 22:06:19 -0800

    added gitignore and fix next function in midlware

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ var app = express();
 //normaliy port should be saved in env var.
 var port = process.env.PORT || 3000;
 
+
+app.use('/', function(req, res, next){
+  console.log('Request Url: ' + req.url);
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.use('/', function(req, res, next){
-  console.log('Request Url: ' + req.url);
-});
-
 app.get('/person/:id', function (req, res) {
   res.send('Hello World! id ' + req.params.id);
-  next();
 });
 //app.use("/assets", express.static(__dirname + '/public'));
 
 
 app.get('/api', function(req, res){
   res.json({firstname:"John", lastName:"Smith"});
-  next();
 });
 
 var server = app.listen(port, function () {
