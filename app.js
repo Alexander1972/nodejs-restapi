9c719c29e85831617b651f289ef0376916a2cc19
Alexander <alexander.matsaylo@gmail.com>
2015-11-30 23:55:37 -0800

    added templates ejs and made a post method and extracted it values from the post request

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 //normaliy port should be saved in env var.
 var port = process.env.PORT || 3000;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set('view engine', 'ejs');
 
 
 app.use('/', function(req, res, next){
@@ -10,13 +15,24 @@ app.use('/', function(req, res, next){
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.get('/person/:id', function (req, res) {
   res.send('Hello World! id ' + req.params.id);
 });
-//app.use("/assets", express.static(__dirname + '/public'));
+
+app.get('/querystring/:id', function (req, res) {
+  res.send('Hello World! id', {id: req.params.id, Qstr: req.query.qstr});
+});
+
+app.post('/person', urlencodedParser, function (req, res) {
+  res.send('Thank you');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
+
+app.use("/assets", express.static(__dirname + '/public'));
 
 
 app.get('/api', function(req, res){
